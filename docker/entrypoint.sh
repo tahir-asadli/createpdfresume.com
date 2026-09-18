4ab61c8291bc3417e223bcef93b30d093ecd5d69
Tahir Asadli <tahir-asadov@outlook.com>
2026-09-18 12:00:33 +0400

    Update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -e
-
+if [ ! -f .env ]; then
+    cp .env.app .env
+fi
 php artisan key:generate
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
-# php artisan migrate --force
+php artisan migrate --force
 
 exec "$@"
