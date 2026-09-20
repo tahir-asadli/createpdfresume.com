804a18ddf66fe9bedec02daa92943c022994cd07
Tahir Asadli <tahir-asadov@outlook.com>
2026-09-20 09:11:55 +0400

    Update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,6 +4,7 @@ if [ ! -f .env ]; then
     cp .env.app .env
 fi
 php artisan key:generate
+php artisan storage:link
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
--- src/app/Models/Resume.php
+++ src/app/Models/Resume.php
@@ -82,6 +82,7 @@ class Resume extends Model
             }
             file_put_contents($page_html_file_path, $page_html_content);
             $command = "google-chrome --no-sandbox --headless --disable-gpu --print-to-pdf=\"$page_pdf_file_path\" $page_html_file_path";
+            info($command);
             $command_result = Process::run($command);
             if ($command_result->successful()) {
                 $pages_pdf_files .= $page_pdf_file_path . ' ';
