784fbf2dc53feb1cdd68f15d509e77c765867bda
Tahir Asadli <tahir-asadov@outlook.com>
2026-09-21 09:09:33 +0400

    Update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 set -e
-if [ ! -f .env ]; then
-    cp .env.app .env
-fi
+# if [ ! -f .env ]; then
+    # cp .env.app .env
+# fi
 
 
-cp -r ./app_data/ /var/www/html/storage/app
+# cp -r ./app_data/. /var/www/html/storage/app
+# docker cp ./app_data/. createpdfresumecom-app-1:/var/www/html/storage/app
+
 # php artisan key:generate
 # php artisan storage:link
 # php artisan config:cache
 # php artisan route:cache
 # php artisan view:cache
 php artisan migrate --force
+
+supervisorctl restart all
 
 exec "$@"
